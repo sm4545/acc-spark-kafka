804a20ff6743d33d966b56e509f54202ca3d6a1b
sm4545 <56088725+sm4545@users.noreply.github.com>
2021-05-31 16:48:56 -0400

    Update spark_print.py

--- spark_print.py
+++ spark_print.py
@@ -8,20 +8,20 @@ df = spark \
 .format("kafka") \
 .option("kafka.bootstrap.servers", "172.25.0.12:9092,172.25.0.13:9092") \
 .option("startingOffsets", "earliest") \
-.option("subscribe", "AAPL_TOPIC_10") \
+.option("subscribe", "GME_TOPIC_1") \
 .load()
 
 df1 = df.selectExpr("CAST(key AS STRING)", "CAST(CAST(value AS STRING) AS FLOAT)", "timestamp")
-
 df1.show()
 
 df1.registerTempTable("stockprice_table")
-max_val = spark.sql("SELECT value as HIGHEST_PRICE_ON_18_MAY, key as TIME FROM stockprice_table WHERE value IN (select MAX(value) FROM stockprice_table)")
-max_val.show()
 
-min_val = spark.sql("SELECT value as LOWEST_PRICE_ON_18_MAY, key as TIME FROM stockprice_table WHERE value = (select MIN(value) FROM stockprice_table)")
-min_val.show()
+open_val = spark.sql("SELECT value as OPENING_STOCK_PRICE, key as TIME FROM stockprice_table WHERE value IN (select value FROM stockprice_table WHERE key = '09:31')")
+open_val.show()
 
-std_val = spark.sql("SELECT stddev(value) as STANDARD_DEVIATION FROM stockprice_table")
-std_val.show()
+close_val = spark.sql("SELECT value as CLOSING_STOCK_PRICE, key as TIME FROM stockprice_table WHERE value = (select value FROM stockprice_table WHERE key = '15:59')")
+close_val.show()
+
+# std_val = spark.sql("SELECT stddev(value) as STANDARD_DEVIATION FROM stockprice_table")
+# std_val.show()
                    
